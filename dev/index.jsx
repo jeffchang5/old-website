8907f67db4ef9aa78f0bbaae3c2e2fd5abbb6a80
Jeffrey Chang <changjef@umich.edu>
2017-10-02 23:31:23 -0400

    Environment

---
+++ dev/index.jsx
@@ -0,0 +1,3 @@
+// import React from 'react';
+
+// This is the index file for this Project.
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+//
+module.exports = {
+    entry: path.resolve('./dev/index.jsx'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve('./dist/public')
+    },
+    devtool: 'source-map',
+    target: 'web',
+    devServer: {
+        inline: true,
+        contentBase: path.resolve('./dist'),
+        port: 5000
+    },
+    resolve: {
+        modules: [
+            path.resolve('./dev'),
+            path.resolve('./node_modules')
+        ],
+        extensions: ['.js', '.jsx', '.json']
+    },
+    module: {
+        rules: [
+            { test: /\.jsx$/, enforce: 'pre', loader: 'eslint-loader', options: { emitWarning: true } },
+            { test: /\.jsx?$/, loader: 'babel-loader' },
+            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
+        ]
+    },
+    externals: {
+        'react': 'React',
+        'redux': 'Redux',
+        'react-dom': 'ReactDOM',
+        'react-router-dom': 'ReactRouterDOM',
+        'react-redux': 'ReactRedux'
+    }
+};

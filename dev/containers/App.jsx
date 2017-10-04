c428aa0bb4bc57012504bb77ead1ff2e7d771dfc
Jeffrey Chang <changjef@umich.edu>
2017-10-03 23:23:22 -0400

    Initial dist subtree commit

--- dev/containers/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import NavBar from 'components/navbar';
-import LineSeperator from 'components/common/line_seperator';
-
-export default () =>
-  (<div>
-    <NavBar />
-    <LineSeperator />
-  </div>);
--- dev/theme/global.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-unused-expressions */
-import { injectGlobal } from 'styled-components';
-
-injectGlobal`
-  body {  margin: 0;  };
-  a { color: inherit; text-decoration: none; }
-`;

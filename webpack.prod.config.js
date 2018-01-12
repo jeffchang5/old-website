3b483bf39daabc023219a880bc5092f8887ed005
Jeffrey Chang <me@jeffchang.io>
2018-01-11 23:35:03 -0500

    changes to navbar

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.jsx?$/, loader: 'babel-loader' },
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.(svg|png|jpg|gif|otf|ttf)$/,
         use: [{ loader: 'file-loader',
           options: {

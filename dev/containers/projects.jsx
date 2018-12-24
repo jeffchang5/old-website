69947858ab2a1211bac2e5876226861b2ab88b11
Jeffrey Chang <me@jeffchang.io>
2018-12-23 20:44:04 -0500

    Added sample React carousel

--- dev/containers/projects.jsx
+++ dev/containers/projects.jsx
@@ -1,6 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
 import WideContainer from 'components/common/responsive_container';
+import Slider from "react-slick";
+// import "~slick-carousel/slick/slick.css";
+// import "~slick-carousel/slick/slick-theme.css";
+
+class SimpleSlider extends React.Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    );
+  }
+}
 
 export default class ProjectsComponent extends React.Component {
   constructor(props) {
@@ -17,6 +54,7 @@ export default class ProjectsComponent extends React.Component {
   render() {
     return (
       <WideContainer ref={(section) => { this.section = section; }}>
+        <SimpleSlider />
       </WideContainer>
     );
   }
